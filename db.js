3386db204b07b4759b7f45ddebe049cd03828aed
leonel21Sedano <sedano.leonel@alumnos.udg.mx>
2025-11-23 22:46:53 -0600

    Update database connection settings in db.js

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'mi-db-p-final.cl0kqmkkoncm.us-east-1.rds.amazonaws.com:3306',
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'root',
+  user: process.env.DB_USER || 'admin',
+  password: process.env.DB_PASSWORD || 'hola123mundo',
   database: process.env.DB_NAME || 'cutaloja',
   waitForConnections: true,
   connectionLimit: 10,
